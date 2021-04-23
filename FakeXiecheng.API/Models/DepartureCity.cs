d211a0724a626a2ee12d234a1cdcc4d20a584e27
VictorWu <happyvictorwu@outlook.com>
2021-04-23 15:19:58 +0800

    3-10 更新数据库
    Migrations文件夹也要上传到git，是重要的数据库历史记录
    
    添加数据库字段，更新数据库
    
    数据库中有一个__EFMigrationsHistory的表，efc用来记录数据库的历史记录的，自动维护

---
+++ FakeXiecheng.API/Models/DepartureCity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FakeXiecheng.API.Models
+{
+    public enum DepartureCity
+    {
+        Beijing,
+        Shanghai,
+        Canton, 
+        Shenzhen
+    }
+}
---
+++ FakeXiecheng.API/Models/TripType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FakeXiecheng.API.Models
+{
+    public enum TripType
+    {
+        HotelAndAttractions,
+        Group,
+        PrivateGroup,
+        BackPackTour,
+        SemiBackPackTour
+    }
+}
