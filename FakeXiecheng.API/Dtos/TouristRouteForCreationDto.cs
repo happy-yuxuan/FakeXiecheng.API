15eb0e0d13abc45cc0d3ac8120408a2b750b7817
VictorWu <happyvictorwu@outlook.com>
2021-04-27 11:13:59 +0800

    7-6 添加数据验证
    在Dto中添加数据验证，隔离数据库，增加安全性
    通过引入System.ComponentModel.DataAnnotations;添加Annotation。实现数据验证

--- FakeXiecheng.API/Dtos/TouristRouteForCreationDto.cs
+++ FakeXiecheng.API/Dtos/TouristRouteForCreationDto.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using FakeXiecheng.API.Dtos;
 
 namespace FakeXiecheng.API.Controllers
 {
     public class TouristRouteForCreationDto
     {
+        [Required(ErrorMessage = "title 不可为空")]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(1500)]
         public string Description { get; set; }
         // 计算方式：原价 * 折扣
         public decimal Price { get; set; }
