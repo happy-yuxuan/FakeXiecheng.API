d45be190305dde6bf5afc25416fce6cd76311d34
VictorWu <happyvictorwu@outlook.com>
2021-04-27 15:00:24 +0800

    8-3 PUT请求的数据验证
    对put更新做数据验证，为了与创建的Attribute一致，通过继承同一个父类，在Validation中对父类进行验证

--- FakeXiecheng.API/ValidationAttributes/TouristRouteTitleMustBeDifferentFromDescriptionAttribute.cs
+++ FakeXiecheng.API/ValidationAttributes/TouristRouteTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using FakeXiecheng.API.Controllers;
+using FakeXiecheng.API.Dtos;
 
 namespace FakeXiecheng.API.ValidationAttributes
 {
@@ -10,7 +11,7 @@ namespace FakeXiecheng.API.ValidationAttributes
             object value,
             ValidationContext validationContext)
         {
-            var touristRouteDto = (TouristRouteForCreationDto)validationContext.ObjectInstance;
+            var touristRouteDto = (TouristRouteForManipulationDto)validationContext.ObjectInstance;
             if (touristRouteDto.Title == touristRouteDto.Description)
             {
                 return new ValidationResult(
